e23ac1f45a2ed4b8c2f59aa7c2100697b0fe9119
brandonamos <bpamos21@gmail.com>
2022-11-01 20:44:21 -0600

    add vars for crdb

--- modules-cluster1.tf
+++ modules-cluster1.tf
@@ -222,21 +222,25 @@ module "create-crdbs" {
   providers = {
       aws = aws.a
     }
-  #ssh_key_path         = var.ssh_key_path1
-  #region               = var.region1
+
   re_cluster_username  = var.re_cluster_username
   re_cluster_password  = var.re_cluster_password
-  ### vars pulled from previous modules
-  #vpc_name             = module.vpc1.vpc-name
-  #re-node-internal-ips = module.nodes1.re-data-node-internal-ips
-  #re-node-eip-ips      = module.nodes1.re-data-node-eips
-  #re-data-node-eip-public-dns   = module.nodes1.re-data-node-eip-public-dns
-  dns_fqdn1             = module.dns1.dns-ns-record-name
-  dns_fqdn2             = module.dns2.dns-ns-record-name #getting cluster2 name from other module
-  
-  depends_on           = [module.vpc1, 
-                          module.nodes1, 
-                          module.dns1, 
+  dns_fqdn1            = module.dns1.dns-ns-record-name
+  dns_fqdn2            = module.dns2.dns-ns-record-name #getting cluster2 name from other module
+  crdb_db_name         = var.crdb_db_name
+  crdb_port            = var.crdb_port
+  crdb_memory_size     = var.crdb_memory_size
+  crdb_replication     = var.crdb_replication
+  crdb_aof_policy      = var.crdb_aof_policy
+  crdb_sharding        = var.crdb_sharding
+  crdb_shards_count    = var.crdb_shards_count
+
+  depends_on           = [module.vpc1,
+                          module.vpc2,
+                          module.nodes1,
+                          module.nodes2, 
+                          module.dns1,
+                          module.dns2, 
                           module.create-cluster1, 
                           module.create-cluster2]
 }
@@ -266,18 +270,21 @@ module "memtier_benchmark1" {
   providers = {
       aws = aws.a
     }
-  test-node-count    = var.test-node-count
-  vpc_name           = module.vpc1.vpc-name
-  ssh_key_path       = var.ssh_key_path1
-  crdb_port          = var.crdb_port
-  ### vars pulled from previous modules
-  crdb_endpoint_cluster  = module.create-crdbs.crdb_endpoint_cluster1
-  #crdb_endpoint_cluster2  = module.create-crdbs.crdb_endpoint_cluster2
+  test-node-count           = var.test-node-count
+  vpc_name                  = module.vpc1.vpc-name
+  ssh_key_path              = var.ssh_key_path1
+  crdb_port                 = var.crdb_port
+  crdb_endpoint_cluster     = module.create-crdbs.crdb_endpoint_cluster1
   memtier_data_load_cluster = var.memtier_data_load_cluster1
+  memtier_benchmark_cmd     = var.memtier_benchmark_cluster1
   
-  depends_on           = [module.vpc1, 
-                          module.nodes1, 
-                          module.dns1, 
+  depends_on           = [module.vpc1,
+                          module.vpc2,
+                          module.nodes1,
+                          module.nodes2, 
+                          module.dns1,
+                          module.dns2,
                           module.create-cluster1, 
-                          module.create-cluster2]
+                          module.create-cluster2,
+                          module.create-crdbs]
 }
