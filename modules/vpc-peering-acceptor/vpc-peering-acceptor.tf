70ae97de5bda2a8b80a8055d9a09716be1477496
brandonamos <bpamos21@gmail.com>
2022-11-01 21:26:21 -0600

    clean up

--- modules/vpc-peering-acceptor/vpc-peering-acceptor.tf
+++ modules/vpc-peering-acceptor/vpc-peering-acceptor.tf
@@ -1,11 +1,7 @@
+#### VPC Accepter, takes vpc peering connection id and auto accepts
+#### this is using the provider of the accepter account or region (ie. cluster 2)
 
-# data "aws_caller_identity" "current2" {}
-
-# output "account_id" {
-#   value = data.aws_caller_identity.current2.account_id
-# }
-
-# Accepter's side of the connection.
+### Accepter's side of the connection.
 resource "aws_vpc_peering_connection_accepter" "peer" {
   vpc_peering_connection_id = var.vpc_peering_connection_id
   auto_accept               = true
