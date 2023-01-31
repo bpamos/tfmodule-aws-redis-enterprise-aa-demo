c9a8488ff1247395ea88e64ecbbb3cca06869905
brandonamos <bpamos21@gmail.com>
2023-01-31 15:11:31 -0700

    added rza and rof options

--- modules-cluster1.tf
+++ modules-cluster1.tf
@@ -187,6 +187,8 @@ module "create-cluster1" {
   region               = var.region1
   re_cluster_username  = var.re_cluster_username
   re_cluster_password  = var.re_cluster_password
+  flash_enabled        = var.flash_enabled
+  rack_awareness       = var.rack_awareness
   ### vars pulled from previous modules
   vpc_name             = module.vpc1.vpc-name
   re-node-internal-ips = module.nodes1.re-data-node-internal-ips
