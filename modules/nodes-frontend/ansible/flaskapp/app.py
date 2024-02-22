74b9a710e6bc310892273762a75bae86b4897646
bpamos <bpamos21@gmail.com>
2024-02-22 14:39:48 -0700

    WIP

--- modules/nodes-frontend/ansible/flaskapp/app.py
+++ modules/nodes-frontend/ansible/flaskapp/app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import subprocess
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, world! This is your Flask application. loserrrr'
+    return render_template('index.html')
+
+@app.route('/start_java_app', methods=['POST'])
+def start_java_app():
+    # Start the Java Jedis app as a subprocess
+    subprocess.Popen(['java', '-jar', 'your_java_jedis_app.jar'])
+    return 'Java app started successfully!'
 
 if __name__ == '__main__':
     app.run(debug=True)
